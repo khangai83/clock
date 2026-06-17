9177112bf4cf547ef2f6e15f8ac139f64bc0be81
mac <mac@192.168.1.9>
2026-06-17 11:03:50 +0800

    deploy: 2026-06-17 11:03:50

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2937854968" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1886622620" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
